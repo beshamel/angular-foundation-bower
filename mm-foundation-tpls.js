04f2c7c0bb666a0d2427648c1db4e5b4dabccbf4
artistix <remi.jarasson@gmail.com>
2014-12-06 18:59:14 +0100

    feat(dropdownToggle): dropdown uses container width + close on window resize

--- mm-foundation-tpls.js
+++ mm-foundation-tpls.js
@@ -430,6 +430,8 @@ angular.module('mm.foundation.dropdownToggle', [ 'mm.foundation.position', 'mm.f
       };
 
       var onClick = function (event) {
+        var windowWidth = $window.innerWidth;
+
         dropdown = angular.element($document[0].querySelector(scope.dropdownToggle));
         var elementWasOpen = (element === openElement);
 
@@ -444,27 +446,35 @@ angular.module('mm.foundation.dropdownToggle', [ 'mm.foundation.position', 'mm.f
           dropdown.css('display', 'block'); // We display the element so that offsetParent is populated
           var offset = $position.offset(element);
           var parentOffset = $position.offset(angular.element(dropdown[0].offsetParent));
-          var dropdownWidth = dropdown.prop('offsetWidth');
           var css = {
             top: offset.top - parentOffset.top + offset.height + 'px'
           };
 
           if (controller.small()) {
-            css.left = Math.max((parentOffset.width - dropdownWidth) / 2, 8) + 'px';
             css.position = 'absolute';
             css.width = '95%';
             css['max-width'] = 'none';
           }
+          else
+          {
+            css.width = '';
+            css.position = '';
+            css['max-width'] = '';
+          }
+          dropdown.css(css);
+          var dropdownWidth = dropdown.prop('offsetWidth');
+
+          if (controller.small()) {
+            css.left = Math.max((parentOffset.width - dropdownWidth) / 2, 8) + 'px';
+          }
           else {
             var left = Math.round(offset.left - parentOffset.left);
-            var rightThreshold = $window.innerWidth - dropdownWidth - 8;
+            var rightThreshold = dropdown[0].offsetParent.offsetWidth - dropdownWidth - 8;
             if (left > rightThreshold) {
                 left = rightThreshold;
                 dropdown.removeClass('left').addClass('right');
             }
             css.left = left + 'px';
-            css.position = null;
-            css['max-width'] = null;
           }
 
           dropdown.css(css);
@@ -476,7 +486,11 @@ angular.module('mm.foundation.dropdownToggle', [ 'mm.foundation.position', 'mm.f
           openElement = element;
 
           closeMenu = function (event) {
+            if (event && event.type == 'resize' && event.target.innerWidth == windowWidth) {
+              return;
+            }
             $document.off('click', closeMenu);
+            angular.element($window).unbind('resize', closeMenu);
             dropdown.css('display', 'none');
             closeMenu = angular.noop;
             openElement = null;
@@ -485,6 +499,7 @@ angular.module('mm.foundation.dropdownToggle', [ 'mm.foundation.position', 'mm.f
             }
           };
           $document.on('click', closeMenu);
+          angular.element($window).bind('resize', closeMenu);
         }
       };
 
