6e2f4b90de7af506b4e1b9533afafd29404d7dd5
artistix <remi.jarasson@gmail.com>
2014-12-19 14:05:54 +0100

    feat(offcanvas): broadcast events

--- mm-foundation.js
+++ mm-foundation.js
@@ -2,7 +2,7 @@
  * angular-mm-foundation
  * http://pineconellc.github.io/angular-foundation/
 
- * Version: 0.6.0-SNAPSHOT - 2014-12-06
+ * Version: 0.6.0-SNAPSHOT - 2014-12-19
  * License: MIT
  * (c) Pinecone, LLC
  */
@@ -1294,62 +1294,85 @@ angular.module("mm.foundation.offcanvas", [])
                 $scope.$on('$destroy', function() {
                     win.unbind("resize.body", $scope.hide);
                 });
-
             },
             controller: ['$scope', function($scope) {
 
+                this.leftClose = function() {
+                  $scope.sidebar.removeClass("move-right");
+                };
+
+                this.leftOpen = function() {
+                  $scope.sidebar.removeClass("move-left");
+                  $scope.sidebar.addClass("move-right");
+                };
+
                 this.leftToggle = function() {
+                    $scope.sidebar.removeClass("move-left");
                     $scope.sidebar.toggleClass("move-right");
                 };
 
+                this.rightClose = function() {
+                    $scope.sidebar.removeClass("move-left");
+                };
+
+                this.rightOpen = function() {
+                    $scope.sidebar.removeClass("move-right");
+                    $scope.sidebar.addClass("move-left");
+                };
+
                 this.rightToggle = function() {
+                    $scope.sidebar.removeClass("move-right");
                     $scope.sidebar.toggleClass("move-left");
                 };
 
                 this.hide = function() {
                     $scope.hide();
                 };
+
+                $scope.$on('offCanvas.leftOpen', this.leftOpen);
+                $scope.$on('offCanvas.leftClose', this.leftClose);
+                $scope.$on('offCanvas.leftToggle', this.leftToggle);
+                $scope.$on('offCanvas.rightOpen', this.rightOpen);
+                $scope.$on('offCanvas.rightClose', this.rightClose);
+                $scope.$on('offCanvas.rightToggle', this.rightToggle);
+                $scope.$on('offCanvas.hide', this.hide);
             }]
         };
     }])
-    .directive('leftOffCanvasToggle', [function () {
+    .directive('leftOffCanvasToggle', ['$rootScope', function ($rootScope) {
         return {
-            require: '^offCanvasWrap',
             restrict: 'C',
-            link: function ($scope, element, attrs, offCanvasWrap) {
+            link: function ($scope, element, attrs) {
                 element.on('click', function () {
-                    offCanvasWrap.leftToggle();
+                    $rootScope.$broadcast('offCanvas.leftToggle');
                 });
             }
         };
     }])
-    .directive('rightOffCanvasToggle', [function () {
+    .directive('rightOffCanvasToggle', ['$rootScope', function ($rootScope) {
         return {
-            require: '^offCanvasWrap',
             restrict: 'C',
-            link: function ($scope, element, attrs, offCanvasWrap) {
+            link: function ($scope, element, attrs) {
                 element.on('click', function () {
-                    offCanvasWrap.rightToggle();
+                  $rootScope.$broadcast('offCanvas.rightToggle');
                 });
             }
         };
     }])
-       .directive('exitOffCanvas', [function () {
+    .directive('exitOffCanvas', ['$rootScope', function ($rootScope) {
         return {
-            require: '^offCanvasWrap',
             restrict: 'C',
-            link: function ($scope, element, attrs, offCanvasWrap) {
+            link: function ($scope, element, attrs) {
                 element.on('click', function () {
-                    offCanvasWrap.hide();
+                  $rootScope.$broadcast('offCanvas.hide');
                 });
             }
         };
     }])
-    .directive('offCanvasList', [function () {
+    .directive('offCanvasList', ['$rootScope', function ($rootScope) {
         return {
-            require: '^offCanvasWrap',
             restrict: 'C',
-            link: function ($scope, element, attrs, offCanvasWrap) {
+            link: function ($scope, element, attrs) {
                 element.find('li').on('click', function (e) {
                     e.stopPropagation();
                     if (angular.element(this).hasClass('has-submenu')) {
@@ -1359,7 +1382,7 @@ angular.module("mm.foundation.offcanvas", [])
                         angular.element(this.parentElement).removeClass('move-right');
                         angular.element(this.parentElement).removeClass('move-left');
                     } else {
-                        offCanvasWrap.hide();
+                      $rootScope.$broadcast('offCanvas.hide');
                     }
                 });
             }
